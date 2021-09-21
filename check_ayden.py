96ede7febde437b98abb446115f625af40545a54
Diego Martin Gardella <diegomartin.gardella@capside.com>
2021-09-21 16:35:18 +0200

    Sync

--- check_ayden.py
+++
@@ -1,7 +0,0 @@
-import requests
-from xml.etree import ElementTree
-url = 'https://pal-live.adyen.com/pal/servlet/soap/Recurring/listRecurringDetails'
-data = '{"recurring": {"contract": "RECURRING"},"shopperReference": "pmoran@nubico.es/nubico","merchantAccount": "NubicoES"}'
-response = requests.post(url, data=data,headers={"Content-Type": "application/json"},auth=('ws@Company.Yadican', 'bz=[437x/ncEFE3wdHHI@ik~r'))
-print(response)
-print(response.text)
